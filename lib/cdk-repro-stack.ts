671335d3c2d4a97bb25903330dd2b10ace9d3e58
Nick Hudkins <nhudkins@amazon.com>
2024-07-31 18:24:46 -0400

    ...

--- lib/cdk-repro-stack.ts
+++ lib/cdk-repro-stack.ts
@@ -1,16 +1,32 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
 
-export class CdkReproStack extends cdk.Stack {
+import { ServicePrincipal } from "aws-cdk-lib/aws-iam";
+import { Secret } from "aws-cdk-lib/aws-secretsmanager";
+import { Construct } from "constructs";
+
+export class GoodReproStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const secret = new Secret(this, "something-else", {
+      secretName: "my-secret-name",
+      description: "My Great Secret",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkReproQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    secret.grantRead(new ServicePrincipal("some.service.principal"));
+  }
+}
+
+export class BadReproStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const secret = Secret.fromSecretCompleteArn(
+      this,
+      "something",
+      "arn:aws:secretsmanager:us-east-1:000000000000:secret:the-secret-name-ABC123"
+    );
+
+    secret.grantRead(new ServicePrincipal("some.service.principal"));
   }
 }
